b43ac956d6f61b6fbaaee1048355144ef4ace961
StellaR0511 <81784995+StellaR0511@users.noreply.github.com>
2021-08-09 14:45:30 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+var compoundwords1 = "Compound ";
+var compoundwords2 = "Words";
+var compoundwordsFull = compoundwords1.concat(compoundwords2);
+console.log(compoundwordsFull);
+
+//watermelon//
+var watermelon1 = "Water";
+var watermelon2 = "melon";
+var watermelonFull = watermelon1.concat(watermelon2);
+console.log(watermelonFull);
+
+//rainbow//
+var rainbow1 = "Rain";
+var rainbow2 = "bow";
+var rainbowFull = rainbow1.concat(rainbow2);
+console.log(rainbowFull);
+
+//snowman//
+var snowman1 = "Snow";
+var snowman2 = "man";
+var snowmanFull = snowman1.concat(snowman2);
+console.log(snowmanFull)
+
+var math1 = "Ma";
+var math2 = "th";
+var mathFull = math1.concat(math2);
+console.log(mathFull)
+
+//square root: 841//
+console.log(Math.sqrt(841));
+
+//7 to the power of 9//
+console.log(Math.pow(7,9));
